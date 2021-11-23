81f455d91b09563173756fde3ef2a1eaa75baf5f
Brenda Kiptim <93176526+brendakiptim@users.noreply.github.com>
2021-11-23 15:05:28 +0300

    Add files via upload

---
+++ for-loop-sum-20-25.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int sum = 0;
+    for (int num = 20; num <= 25; num++)
+    {
+        sum = sum + num;
+    }
+    cout << "Total sum is " << sum << "\n";
+    return 0;
+}
